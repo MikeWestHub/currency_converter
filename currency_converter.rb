7dc13824ce4b237cc292bc4d41156c978626a917
Mike West <westmd23@gmail.com>
2016-03-13 21:40:02 -0400

    Setting instance variables and methods for class CurrencyConversion.

---
+++ currency_converter.rb
@@ -0,0 +1,13 @@
+class CurrenyConversion
+
+  def initialize(symbol, amount)
+    @symbol = symbol
+    @amount = amount
+  end
+
+  def conversion_rate
+    @counversion_rate = conversion_rate
+    conversion_rate = {"$" => :USD, "£" => :EUR, "¥" => :JPY}
+  end
+
+end 
