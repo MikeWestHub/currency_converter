fb08f7634caa5fc359911e51c64595a86f530a19
Mike West <westmd23@gmail.com>
2016-03-15 07:51:53 -0400

    Restructureconversion methods as if else statements

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,13 +1,32 @@
 class CurrenyConversion
 
-  def initialize(symbol, amount)
+  def initialize(*money_string)
     @symbol = symbol
     @amount = amount
-  end                             # => :initialize
+    @currency = currency
+  end
+
+  def dollar
+      if :EUR
+      elsif :YEN
+      else
+  end
+
+  def euro
+    if :USD
+    elsif :YEN
+    else
+  end
+
+  def yen
+    if :USD
+    elsif :EUR
+    else
+  end
 
   def conversion_rate
     @counversion_rate = conversion_rate
     conversion_rate = {:USD => 1.00, :EUR => 0.74, :JPY => 120.0}
-  end                                                              # => :conversion_rate
+  end
 
-end  # => :conversion_rate
+end
