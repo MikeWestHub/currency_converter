d3a2888d0cd7a003763474d9f96c8832206aeca3
Mike West <westmd23@gmail.com>
2016-03-14 21:19:33 -0400

    Clean up methods inside of Currency class

--- currency.rb
+++ currency.rb
@@ -1,32 +1,27 @@
 class Currency
   def initialize(currency_string)
-    split_currency = currency_string.split("")  # => ["$", "1", ".", "0", "0"], ["£", "0", ".", "7", "4"], ["¥", "1", "2", "0", ".", "0"]
-    @symbol = split_currency.shift              # => "$", "£", "¥"
-    @amount = split_currency.join("").to_f      # => 1.0, 0.74, 120.0
-  end                                           # => :initialize
+    split_currency = currency_string.chars  # => ["$", "1", "3", ".", "0", "0"], ["$", "1", "0", ".", "0", "0"], ["$", "1", "2", "0", ".", "0", "0"]
+    @symbol = split_currency.shift          # => "$", "$", "$"
+    @amount = split_currency.join("").to_f  # => 13.0, 10.0, 120.0
+  end                                       # => :initialize
 
   def symbol
     symbol_hash = {"$" => :USD, "£" => :EUR, "¥" => :JPY}
-    @currency_code = symbol_hash[@symbol]
+    @symbol_hash = symbol_hash
   end                                                      # => :symbol
-
-  def currency_code
-    @currency_code
-  end                # => :currency_code
-
-  def amount
-    @amount
-  end         # => :amount
 
   def to_s
     "#{@symbol}#{@amount}"
   end                       # => :to_s
 end                         # => :to_s
 
-dollar = Currency.new("$1.00")  # => #<Currency:0x007fec4a1e22b8 @symbol="$", @amount=1.0>
-pound = Currency.new("£0.74")   # => #<Currency:0x007fec4a1e0cb0 @symbol="£", @amount=0.74>
-yen = Currency.new("¥120.0")    # => #<Currency:0x007fec4a1e3960 @symbol="¥", @amount=120.0>
+c1 = Currency.new("$13.00")   # => #<Currency:0x007ff87b897f08 @symbol="$", @amount=13.0>
+c2 = Currency.new("$10.00")   # => #<Currency:0x007ff87b897198 @symbol="$", @amount=10.0>
+c2 = Currency.new("$120.00")  # => #<Currency:0x007ff87b896658 @symbol="$", @amount=120.0>
 
+puts c1 + c2 + c3  # ~> NoMethodError: undefined method `+' for #<Currency:0x007ff87b897f08 @symbol="$", @amount=13.0>
 
-
-# >>
+# ~> NoMethodError
+# ~> undefined method `+' for #<Currency:0x007ff87b897f08 @symbol="$", @amount=13.0>
+# ~>
+# ~> /Users/mikewest/Desktop/class/currency_converter/currency.rb:22:in `<main>'
