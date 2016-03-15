c4e7c1bd24886cdd56eb1acd92c6fd766587c77f
Mike West <westmd23@gmail.com>
2016-03-14 22:32:04 -0400

    Attempting to get sum of added values to apper with symbol

--- currency.rb
+++ currency.rb
@@ -1,25 +1,26 @@
 class Currency
   def initialize(currency_string)
-    split_currency = currency_string.chars  # => ["$", "1", "3", ".", "0", "0"], ["$", "1", "0", ".", "0", "0"], ["$", "1", "2", "0", ".", "0", "0"]
-    @symbol = split_currency.shift          # => "$", "$", "$"
-    @amount = split_currency.join           # => "13.00", "10.00", "120.00"
-  end                                       # => :initialize
+    split_currency = currency_string.chars
+    @symbol = split_currency.shift
+    @amount = split_currency.join
+  end
 
   def to_f
-    @amount.to_f  # => 13.0, 10.0, 120.0
-  end             # => :to_f
+    @amount.to_f
+  end
 
   def to_s
-    "#{@symbol}#{@amount}"
-  end                       # => :to_s
-end                         # => :to_s
+    amount_f = @amount.to_f
+    "#{@symbol}#{amount_f}"
+  end
+end
 
-c1 = Currency.new("$13.00")   # => #<Currency:0x007f9fd60240e0 @symbol="$", @amount="13.00">
-c2 = Currency.new("$10.00")   # => #<Currency:0x007f9fd601fc48 @symbol="$", @amount="10.00">
-c3 = Currency.new("$120.00")  # => #<Currency:0x007f9fd601f018 @symbol="$", @amount="120.00">
+#examples below for class creation and format for addition
 
-currency_instance_total= c1.to_f + c2.to_f + c3.to_f  # => nil
+c1 = Currency.new("$13.00")
+c2 = Currency.new("$10.00")
+c3 = Currency.new("$120.00")
+
+currency_instance_total= c1.to_f + c2.to_f + c3.to_f
 puts currency_instance_total.to_s
 puts [c1, c2, c3]
-
-# >> 143.0
