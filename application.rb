88450ca619628d178cd5da0546cf71a041a44dce
Mike West <westmd23@gmail.com>
2016-03-14 22:21:59 -0400

    Working on building addition of currency

--- application.rb
+++ application.rb
@@ -2,7 +2,14 @@ require_relative "currency"
 require_relative "currency_converter"
 
 
-argument = true
-first_currency = Currency.new(argument)
-second_currency = Currency.new (argument)
-#this is where classes will be called
+c1 = Currency.new("¥120.00")
+c2 = Currency.new("¥1,000.00")
+
+currency_total = c1.to_f + c2.to_f
+puts currency_total
+
+# def symbol
+#   currency_code = true
+#   symbol_hash = {"$" => :USD, "£" => :EUR, "¥" => :JPY}
+#   symbol_hash[currency_code]
+# end
